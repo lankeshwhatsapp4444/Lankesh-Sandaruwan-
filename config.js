3d6658bc096f7b86db63e315e4c93e27f7815626
Kingrasinda <kingrasinda@gmail.com>
2024-10-27 02:41:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=fzQETazI#VxcBL99z7MZOFTUF0Fu2BMNiBIx8ZxmNFSROGMICdfc",
-MONGODB: process.env.MONGODB || "mongodb+srv://ponnapkaya2:CUfG1C4P0RUfFsSk@cluster0.7qsf0.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+MONGODB: process.env.MONGODB || "mongodb+srv://ponnapkaya1:CUfG1C4P0RUfFsSk@cluster0.7qsf0.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
