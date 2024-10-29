196d74083c86a451d4e5d65b0905e7c1478e8635
Danuka123123 <botdanu01@gmail.com>
2024-10-29 14:36:06 +0530

    Update config.js
    𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=GjIEETJI#zmIx1ioKc-qds8li-6HzUFb49oZixduFYWdq1XnQ1rY

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=GjIEETJI#zmIx1ioKc-qds8li-6HzUFb49oZixduFYWdq1XnQ1rY",
+MONGODB: process.env.MONGODB || "mongodb://mongo:ATUdNXYlCCXxOvdqUlXjWObNPwdkdpib@autorack.proxy.rlwy.net:42669",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
