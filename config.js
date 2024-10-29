c1b97c81577672ca1c77b417a28834fd01e1b0c6
Bigmanrashka86 <boymaldini919@gmail.com>
2024-10-29 21:54:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=qu5UVTqS#VZr4fRVvioxXPkJxfNZ7soxjwP2IiczdghQfsbAov48",
+MONGODB: process.env.MONGODB || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=qu5UVTqS#VZr4fRVvioxXPkJxfNZ7soxjwP2IiczdghQfsbAov48",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
