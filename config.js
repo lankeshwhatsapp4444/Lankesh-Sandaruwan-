2ff0ddb0a44a1b2d8fb487abbfb505a493f827c1
AJSWRLD1 <167708025+AJSWRLD1@users.noreply.github.com>
2024-11-02 17:17:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=nuQXyYgD#YNkDwUCWBjP45gmfOZTpIIshQPW3QZ_e11ELvt09uFw",
+MONGODB: process.env.MONGODB || "mongodb+srv://ajswrld123:<db_password>@cluster0.8fahf.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
